c513179b7b51f05d7cd87575bb34157d701ba7da
Cory Hedeen <hedeencory@gmail.com>
2019-04-09 16:16:35 -0400

    Wrote a very basic closure

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,7 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
-
+const testVar = 'String';
+function testFunc(){
+  return testVar;
+}
+console.log(testFunc())
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
 
